1b31b51ab63cb9cda12bbfbe4ddf93854523e0b7
Alec Musasa <alecmus@live.com>
2023-06-13 13:56:07 +0200

    Get list as JSON String

--- src/test/java/com/github/alecmus/spring6resttemplate/client/BeerClientImplTest.java
+++ src/test/java/com/github/alecmus/spring6resttemplate/client/BeerClientImplTest.java
@@ -1,12 +1,19 @@
 package com.github.alecmus.spring6resttemplate.client;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class BeerClientImplTest {
+
+    @Autowired
+    BeerClient beerClient;
 
     @Test
     void listBeers() {
+        beerClient.listBeers();
     }
 }
