cb6fa0c08bf4919d10eadf338abd94d71781d973
Alec Musasa <alecmus@live.com>
2023-06-13 17:04:28 +0200

    Spring Pageable with Jackson

--- src/main/java/com/github/alecmus/spring6resttemplate/client/BeerClientImpl.java
+++ src/main/java/com/github/alecmus/spring6resttemplate/client/BeerClientImpl.java
@@ -1,15 +1,13 @@
 package com.github.alecmus.spring6resttemplate.client;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.github.alecmus.spring6resttemplate.model.BeerDTO;
+import com.github.alecmus.spring6resttemplate.model.BeerDTOPageImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.Map;
 
 @RequiredArgsConstructor
 @Service
@@ -24,22 +22,9 @@ public class BeerClientImpl implements BeerClient {
     public Page<BeerDTO> listBeers() {
         RestTemplate restTemplate = restTemplateBuilder.build();
 
-        ResponseEntity<String> stringResponse =
-                restTemplate.getForEntity(BASE_URL + BASE_BEER_PATH, String.class);
+        ResponseEntity<BeerDTOPageImpl> stringResponse =
+                restTemplate.getForEntity(BASE_URL + BASE_BEER_PATH, BeerDTOPageImpl.class);
 
-        ResponseEntity<Map> mapResponse =
-                restTemplate.getForEntity( BASE_URL + BASE_BEER_PATH, Map.class);
-
-        ResponseEntity<JsonNode> jsonResponse =
-                restTemplate.getForEntity( BASE_URL + BASE_BEER_PATH, JsonNode.class);
-
-        jsonResponse.getBody().findPath("content")
-                .elements()
-                .forEachRemaining(node -> {
-                    System.out.println(node.get("beerName").asText());
-                });
-
-        System.out.println(stringResponse.getBody());
 
         return null;
     }
