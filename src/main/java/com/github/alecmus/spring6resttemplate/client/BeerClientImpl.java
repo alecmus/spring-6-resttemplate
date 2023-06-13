f215b64abe95c22be175fa24fa284bf056fd85b1
Alec Musasa <alecmus@live.com>
2023-06-13 15:13:58 +0200

    Get List as Java Map
    Jackson used in the background

--- src/main/java/com/github/alecmus/spring6resttemplate/client/BeerClientImpl.java
+++ src/main/java/com/github/alecmus/spring6resttemplate/client/BeerClientImpl.java
@@ -8,18 +8,26 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Map;
+
 @RequiredArgsConstructor
 @Service
 public class BeerClientImpl implements BeerClient {
 
     private final RestTemplateBuilder restTemplateBuilder;
 
+    private static final String BASE_URL = "http://localhost:8080";
+    private static final String BASE_BEER_PATH = "/api/v1/beer";
+
     @Override
     public Page<BeerDTO> listBeers() {
         RestTemplate restTemplate = restTemplateBuilder.build();
 
         ResponseEntity<String> stringResponse =
-                restTemplate.getForEntity("http://localhost:8080/api/v1/beer", String.class);
+                restTemplate.getForEntity(BASE_URL + BASE_BEER_PATH, String.class);
+
+        ResponseEntity<Map> mapResponse =
+                restTemplate.getForEntity( BASE_URL + BASE_BEER_PATH, Map.class);
 
         System.out.println(stringResponse.getBody());
 
