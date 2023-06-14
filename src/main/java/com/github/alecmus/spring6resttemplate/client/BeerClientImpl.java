dc206e532743cb1714e6873b70f7d86d5d7275cc
Alec Musasa <alecmus@live.com>
2023-06-14 18:44:10 +0200

    HTTP Put with RestTemplate

--- src/main/java/com/github/alecmus/spring6resttemplate/client/BeerClientImpl.java
+++ src/main/java/com/github/alecmus/spring6resttemplate/client/BeerClientImpl.java
@@ -24,9 +24,16 @@ public class BeerClientImpl implements BeerClient {
     private static final String GET_BEER_BY_ID_PATH = "/api/v1/beer/{beerId}";
 
     @Override
+    public BeerDTO updateBeer(BeerDTO beerDto) {
+        RestTemplate restTemplate = restTemplateBuilder.build();
+        restTemplate.put(GET_BEER_BY_ID_PATH, beerDto, beerDto.getId());
+
+        return getBeerById(beerDto.getId());
+    }
+
+    @Override
     public BeerDTO createBeer(BeerDTO newDto) {
         RestTemplate restTemplate = restTemplateBuilder.build();
-
         URI uri = restTemplate.postForLocation(GET_BEER_PATH, newDto);
         return restTemplate.getForObject(uri.getPath(), BeerDTO.class);
     }
