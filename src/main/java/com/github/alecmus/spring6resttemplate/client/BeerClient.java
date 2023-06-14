d131bebe3c669cedc6f6e8ee6320e7d0bb0d190f
Alec Musasa <alecmus@live.com>
2023-06-14 10:29:33 +0200

    Added getBeerById to demonstrate URL parameters

--- src/main/java/com/github/alecmus/spring6resttemplate/client/BeerClient.java
+++ src/main/java/com/github/alecmus/spring6resttemplate/client/BeerClient.java
@@ -4,9 +4,13 @@ import com.github.alecmus.spring6resttemplate.model.BeerDTO;
 import com.github.alecmus.spring6resttemplate.model.BeerStyle;
 import org.springframework.data.domain.Page;
 
+import java.util.UUID;
+
 public interface BeerClient {
 
     Page<BeerDTO> listBeers();
     Page<BeerDTO> listBeers(String beerName, BeerStyle beerStyle, Boolean showInventory, Integer pageNumber,
                             Integer pageSize);
+
+    BeerDTO getBeerById(UUID beerId);
 }
--- src/test/java/com/github/alecmus/spring6resttemplate/client/BeerClientImplTest.java
+++ src/test/java/com/github/alecmus/spring6resttemplate/client/BeerClientImplTest.java
@@ -1,8 +1,10 @@
 package com.github.alecmus.spring6resttemplate.client;
 
+import com.github.alecmus.spring6resttemplate.model.BeerDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,6 +13,18 @@ class BeerClientImplTest {
 
     @Autowired
     BeerClient beerClient;
+
+    @Test
+    void getBeerById() {
+
+        Page<BeerDTO> beerDTOS = beerClient.listBeers();
+
+        BeerDTO dto = beerDTOS.getContent().get(0);
+
+        BeerDTO byId = beerClient.getBeerById(dto.getId());
+
+        assertNotNull(byId);
+    }
 
     @Test
     void listBeersNoBeerName() {
