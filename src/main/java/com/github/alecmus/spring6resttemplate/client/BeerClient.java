81e8df82d4b3d194a3b8f3e30818a8d5ae72e7bf
Alec Musasa <alecmus@live.com>
2023-06-14 09:19:10 +0200

    Added the rest of the query parameters

--- src/main/java/com/github/alecmus/spring6resttemplate/client/BeerClient.java
+++ src/main/java/com/github/alecmus/spring6resttemplate/client/BeerClient.java
@@ -1,8 +1,12 @@
 package com.github.alecmus.spring6resttemplate.client;
 
 import com.github.alecmus.spring6resttemplate.model.BeerDTO;
+import com.github.alecmus.spring6resttemplate.model.BeerStyle;
 import org.springframework.data.domain.Page;
 
 public interface BeerClient {
-    Page<BeerDTO> listBeers(String beerName);
+
+    Page<BeerDTO> listBeers();
+    Page<BeerDTO> listBeers(String beerName, BeerStyle beerStyle, Boolean showInventory, Integer pageNumber,
+                            Integer pageSize);
 }
--- src/test/java/com/github/alecmus/spring6resttemplate/client/BeerClientImplTest.java
+++ src/test/java/com/github/alecmus/spring6resttemplate/client/BeerClientImplTest.java
@@ -14,11 +14,13 @@ class BeerClientImplTest {
 
     @Test
     void listBeersNoBeerName() {
-        beerClient.listBeers(null);
+
+        beerClient.listBeers(null, null, null, null, null);
     }
 
     @Test
     void listBeers() {
-        beerClient.listBeers("ALE");
+
+        beerClient.listBeers("ALE", null, null, null, null);
     }
 }
