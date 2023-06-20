ecc78f399487eaa07495ba5275733809e1f83fa1
Alec Musasa <alecmus@live.com>
2023-06-20 19:35:40 +0200

    Setup HTTP Basic with RestTemplate

--- src/main/java/com/github/alecmus/spring6resttemplate/config/RestTemplateBuilderConfig.java
+++ src/main/java/com/github/alecmus/spring6resttemplate/config/RestTemplateBuilderConfig.java
@@ -13,6 +13,12 @@ public class RestTemplateBuilderConfig {
     @Value("${rest.template.rootUrl}")
     String rootUrl;
 
+    @Value("${rest.template.username}")
+    String username;
+
+    @Value("${rest.template.password}")
+    String password;
+
     @Bean
     RestTemplateBuilder restTemplateBuilder(RestTemplateBuilderConfigurer configurer) {
 
@@ -22,6 +28,8 @@ public class RestTemplateBuilderConfig {
         DefaultUriBuilderFactory uriBuilderFactory = new
                 DefaultUriBuilderFactory(rootUrl);
 
-        return builder.uriTemplateHandler(uriBuilderFactory);
+        RestTemplateBuilder builderWithAuth = builder.basicAuthentication(username, password);
+
+        return builderWithAuth.uriTemplateHandler(uriBuilderFactory);
     }
 }
